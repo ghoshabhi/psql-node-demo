ccbe4361c141f5afeaa0aa9040c11a80c3c209c5
Abhishek Ghosh <abhighosh18@gmail.com>
2017-04-25 12:03:03 -0700

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+var pg = require('pg');
+
+app.get('/db', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM demo_table', function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("There was some error: " + err);
+      } else {
+        response.render('pages/db', { results: results.rows});
+      }
+    });
+  });
+});
